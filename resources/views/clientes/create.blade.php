f4b0073e67c1121ae604d0d6e4192d3e7f7b885f
yahirc <40361176+yahirc@users.noreply.github.com>
2018-06-26 21:16:11 -0500

    master

--- resources/views/clientes/create.blade.php
+++ resources/views/clientes/create.blade.php
@@ -78,7 +78,7 @@
                 <div class="card">
                     <div class="card-header" id="headingOne">
                     <h5 class="mb-0">
-                        <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                        <button type="button" class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
                             Medidas
                         </button>
                     </h5>
